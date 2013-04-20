27f6aff4ed18d6a28682b1a3c953a367cb6556ad
Finn Pauls <derfinn@gmail.com>
2013-04-20 14:41:57 +0300

    Answer with image to mentions

--- drone.js
+++ drone.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 tweetingdrone = require('./tweetingdrone').tweetingdrone;
 var td = new tweetingdrone();
 //td.getMentions();
-//td.setupStream();
+td.setupStream();
 //td.tweetImage('testing...', 'logo.png');
 
 var statusFile ='/home/finn/testdata/status.txt'
